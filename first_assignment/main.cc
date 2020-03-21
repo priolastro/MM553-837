ec5d56dec604768b0ab9f15ddc72a298e3494727
Salvatore Prioli <prioli@adm-125099.adm.c.sdu.dk>
2020-03-21 11:49:33 +0100

    cleaning first assignment

--- first_assignment/main.cc
+++ first_assignment/main.cc
@@ -1,5 +1,5 @@
 /*************************
- * Fermi Pasta Ulam Tsingu problem
+ * Spring 
  * ***********************/
 
 #include<iostream>
@@ -11,49 +11,117 @@
 #include<chrono>
 #include<math.h>
 
-#include "integrators.h"
-#include "systems.h"
-
 #define _USE_MATH_DEFINES
 using namespace std;
 using namespace std::chrono;
 
-int main(int argc, char** argv) {
-    if (argc != 5) {
-        cerr << "usage: " << argv[0] << " <t> <niter> <nmeas> <npart> " << endl;
-        return 1;
-    }
+class TwoSpringsOneMasses{
+    double m,k,gamma;
+    public:
+        TwoSpringsOneMasses(const double& m_in, const double& k_in, const double& gamma_in):m(m_in), k(k_in), gamma(gamma_in) {}
 
+        double operator() (const double& q) const {
+            double F=-2*q*k;
+            return F;
+        } 
+};
+
+class ThreeSpringsTwoMasses{
+    double m,k,gamma;
+    public:
+        ThreeSpringsTwoMasses(const double& m_in, const double& k_in):m(m_in), k(k_in) {}
+
+        int GetDofF(vector<double>& q) const{
+            double DofF=q.size();
+            return DofF;
+        };
+
+        double operator() (int i, const vector<double>& q) const {
+            int DofF=q.size();
+            double F;
+            if (DofF==1){ 
+                F=-2*q[i];
+            }
+            else if (DofF==2){
+                if (i==0){
+                    F=-2*q[i]+q[i+1];
+                }
+                else {
+                    F=-2*q[i]+q[i-1];
+                }
+            }
+            else if (DofF>=3){
+                if (i==0){
+                    F=-2*q[i]+q[i+1];
+                }
+                else if (i==DofF-1) {
+                    F=-2*q[i]+q[i-1];
+                }
+                else{
+                    F=-2*q[i]+q[i-1]+q[i+1];
+                }
+            }
+            return F;
+        } 
+};
+
+template<class Force>
+class Leapfrog{
+    double epsilon;
+    const Force& F;
+    vector<double> qn;
+    vector<double> pn;
+    public:
+        Leapfrog(const Force& F_in, const double& e_in): F(F_in), epsilon(e_in){}
+
+        void setInitialCondition(const vector<double>& q0, const vector<double>& p0) {
+            qn=q0, pn=p0;
+        }
+        void operator() (vector<double>& qnp1, vector<double>& pnp1){
+            int ndof=F.GetDofF(qn);
+            qnp1.resize(ndof);
+            pnp1.resize(ndof);
+            vector<double> pnp12(ndof);
+            
+            for (int i=0; i<ndof; i++){
+                pnp12[i]=pn[i]+0.5*epsilon*F(i, qn);
+                qnp1[i]=qn[i]+epsilon*pnp12[i];
+            }
+            for (int i=0; i<ndof; i++){
+                pnp1[i]=pnp12[i]+0.5*epsilon*F(i,qnp1);
+            }
+            qn=qnp1, pn=pnp1;
+            }
+};
+
+int main(int argc, char** argv) {
     double tau;
     stringstream(argv[1]) >> tau;
-    double niter;
+    int niter;
     stringstream(argv[2]) >> niter;
-    double nmeas;
-    stringstream(argv[3]) >> nmeas;
-    double npart;
-    stringstream(argv[4]) >> npart;
+    int npart;
+    stringstream(argv[3]) >> npart;
 
-    double eps = tau / double(niter);
+    double eps=tau/double(niter);
 
-    vector<double> qn(npart);
-    vector<double> pn(npart);
+    ofstream dati("dati.out");
 
-    pn[0] = 0, qn[0] = 0;
-    pn[npart - 1] = 0, qn[npart - 1] = 0;
+    vector<double> qn(npart),pn(npart);
+    for (int n = 0; n < npart; n++) {
+        pn[n] = sin((2*M_PI * (n+0.2) / npart));
+        qn[n] = 0;
+    };
 
-    for (int n = 1; n < npart - 1; n++) {
-        pn[n] = sin((2*M_PI * (n+0.2) / (double) npart));
-    }
+    ThreeSpringsTwoMasses ss(1,1);
+    Leapfrog<ThreeSpringsTwoMasses> integrator(ss, eps);
 
-    SpringSystem ss(qn);
-    LeapfrogIntegrator<SpringSystem> integrator(ss, eps);
-    integrator.setInitialConditions(qn,pn);
-    for (int t = 0; t < niter; t++) {
-        double Potential = calc_Potential(npart, qn);
-        double Kinetic = calc_Kinetic(npart, pn);
-        double p_tot = calc_Average_momentum(npart, pn);
-        cout << Potential << " " << Kinetic << " " << p_tot / npart << endl;
-        integrator(qn, pn);
-    }
-return 0;
+    integrator.setInitialCondition(qn,pn);
+
+    for (int t=0; t<niter; t++){
+        for (int i=0; i<qn.size();++i){
+            dati << qn[i] << " " << pn[i] << endl;
+        }
+        integrator(qn,pn);
+    };
+return 1;
 }
